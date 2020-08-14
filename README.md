f8a4fb8e48fa11bf9aef88c35710ea8006fb199b
Shaleen Jain <shaleen@jain.sh>
2020-08-14 21:55:37 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Currently working on DeFi app on Ethereum and associated projects. [Follow me on Twitter](https://twitter.com/shalzzj).
+
+<table><tr><td valign="top" style="width: 50%;">
+
+### Recent releases (for project)
+<!-- recent_releases starts -->
+<!-- recent_releases ends -->
+</td><td valign="top" style="width: 50%;">
+
+### On my blog
+<!-- blog starts -->
+<!-- blog ends -->
+More on [shaleenjain.com](https://shaleenjain.com/)
+</td></tr></table>
+
+<a href="https://github.com/shalzz/shalzz/actions"><img src="https://github.com/shalzz/shalzz/workflows/Build%20README/badge.svg" align="right" alt="Build README"></a>
---
+++ build_readme.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python3
+# Based on https://github.com/simonw/simonw/
+from python_graphql_client import GraphqlClient
+import feedparser
+import json
+import pathlib
+import re
+import os
+import requests
+
+root = pathlib.Path(__file__).parent.resolve()
+client = GraphqlClient(endpoint="https://api.github.com/graphql")
+
+
+TOKEN = os.environ.get("SHALZZ_TOKEN", "")
+
+
+def replace_chunk(content, marker, chunk):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+def make_query(after_cursor=None):
+    return """
+query {
+  viewer {
+    repositories(first: 100, privacy: PUBLIC, after:AFTER,  orderBy: {field: UPDATED_AT, direction: DESC}) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        nameWithOwner
+        name
+        releases(last:1) {
+          totalCount
+          nodes {
+            name
+            publishedAt
+            url
+          }
+        }
+      }
+    }
+  }
+}
+""".replace(
+        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+    )
+
+
+def fetch_releases(oauth_token):
+    releases = []
+    has_next_page = True
+    after_cursor = None
+
+    while has_next_page:
+        data = client.execute(
+            query=make_query(after_cursor),
+            headers={"Authorization": "Bearer {}".format(oauth_token)},
+        )
+        print()
+        print(json.dumps(data, indent=4))
+        print()
+        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
+            if repo["releases"]["totalCount"]:
+                releases.append(
+                    {
+                        "nameWithOwner": repo["nameWithOwner"],
+                        "release": repo["releases"]["nodes"][0]["name"].replace(repo["name"], "").strip(),
+                        "published_at": repo["releases"]["nodes"][0]["publishedAt"].replace('-','/').split("T")[0],
+                        "url": repo["releases"]["nodes"][0]["url"],
+                    }
+                )
+        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
+            "hasNextPage"
+        ]
+        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
+    
+    releases.sort(key=lambda r: r["published_at"], reverse=True)
+    return releases
+
+
+def fetch_blog_entries():
+    entries = feedparser.parse("https://shaleenjain.com/atom.xml")["entries"]
+    return [
+        {
+            "title": entry["title"],
+            "url": entry["link"].split("#")[0],
+            "published": "%d/%02d/%02d" % (entry.published_parsed.tm_year, entry.published_parsed.tm_mon, entry.published_parsed.tm_mday),
+        }
+        for entry in entries
+    ]
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    releases = fetch_releases(TOKEN)
+    md = "\n".join(
+        [
+            "* [{nameWithOwner} {release}]({url}) - {published_at}".format(**release)
+            for release in releases[:8]
+        ]
+    )
+    readme_contents = readme.open().read()
+    rewritten = replace_chunk(readme_contents, "recent_releases", md)
+
+    entries = fetch_blog_entries()[:5]
+    entries_md = "\n".join(
+        ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
+    )
+    rewritten = replace_chunk(rewritten, "blog", entries_md)
+    
+    readme.open("w").write(rewritten)
